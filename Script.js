96b1f9cf0d652f7adc3855cfa31a3911eaf8a216
NamejsCA <127629061+NamejsCA@users.noreply.github.com>
2023-06-29 19:19:52 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -4,13 +4,91 @@ var map = L.map('map').setView([0, 0], 1); // Set the initial view to center and
 // Create marker groups
 var RisingStarMushroom = L.layerGroup();
 var TheObjectFromEarth = L.layerGroup();
+var GorbaCabbage = L.layerGroup();
+var TravelerPlant = L.layerGroup();
+var GreatfulFrog = L.layerGroup();
 
 // Add markers to group 1
-var markersRisingStarMushroom = [
+var markersGreatfulFrog = [
   {
     latlng: [51.5074, -0.1278],
     popupContent: 'Marker 1',
     imageUrl: 'marker1.png' // Optional image URL for the marker
+  },
+  {
+    latlng: [48.8566, 2.3522],
+    popupContent: 'Marker 2'
+    // imageUrl not specified for this marker
+  }
+];
+markersGreatfulFrog.forEach(function (markerData) {
+  var marker = L.marker(markerData.latlng).bindPopup(getMarkerDescription(markerData.latlng));
+  if (markerData.imageUrl) {
+    marker.on('popupopen', function () {
+      var image = document.createElement('img');
+      image.src = markerData.imageUrl;
+      marker.getPopup().setContent(markerData.popupContent + '<br>' + image.outerHTML);
+    });
+  }
+  GreatfulFrog.addLayer(marker);
+});
+
+// Add markers to group 1
+var markersTravelerPlant = [
+  {
+    latlng: [51.5074, -0.1278],
+    popupContent: 'Marker 1',
+    imageUrl: 'marker1.png' // Optional image URL for the marker
+  },
+  {
+    latlng: [48.8566, 2.3522],
+    popupContent: 'Marker 2'
+    // imageUrl not specified for this marker
+  }
+];
+markersTravelerPlant.forEach(function (markerData) {
+  var marker = L.marker(markerData.latlng).bindPopup(getMarkerDescription(markerData.latlng));
+  if (markerData.imageUrl) {
+    marker.on('popupopen', function () {
+      var image = document.createElement('img');
+      image.src = markerData.imageUrl;
+      marker.getPopup().setContent(markerData.popupContent + '<br>' + image.outerHTML);
+    });
+  }
+  TravelerPlant.addLayer(marker);
+});
+
+// Add markers to group 1
+var markersGorbaCabbage = [
+  {
+    latlng: [51.5074, -0.1278],
+    popupContent: 'Marker 1',
+    imageUrl: 'marker1.png' // Optional image URL for the marker
+  },
+  {
+    latlng: [48.8566, 2.3522],
+    popupContent: 'Marker 2'
+    // imageUrl not specified for this marker
+  }
+];
+markersGorbaCabbage.forEach(function (markerData) {
+  var marker = L.marker(markerData.latlng).bindPopup(getMarkerDescription(markerData.latlng));
+  if (markerData.imageUrl) {
+    marker.on('popupopen', function () {
+      var image = document.createElement('img');
+      image.src = markerData.imageUrl;
+      marker.getPopup().setContent(markerData.popupContent + '<br>' + image.outerHTML);
+    });
+  }
+  GorbaCabbage.addLayer(marker);
+});
+
+// Add markers to group 1
+var markersRisingStarMushroom = [
+  {
+    latlng: [83.7155, 94.3945],
+    popupContent: 'Marker 1',
+    imageUrl: 'https://static.wikia.nocookie.net/fantastic-frontier-roblox/images/2/29/Screen_Shot_2020-01-22_at_5.48.51_pm.png/revision/latest/scale-to-width-down/320?cb=20200122070305' // Optional image URL for the marker
   },
   {
     latlng: [48.8566, 2.3522],
@@ -58,7 +136,10 @@ markersTheObjectFromEarth.forEach(function (markerData) {
 // Create an object to store the marker groups
 var markerGroups = {
   'Rising Star Mushroom': RisingStarMushroom,
-  'The Object From Earth': TheObjectFromEarth
+  'The Object From Earth': TheObjectFromEarth,
+  'Gorbacabbage': GorbaCabbage,
+  'Greatful Frog': GreatfulFrog,
+  'Traveler Plant': TravelerPlant
 };
 
 // Add marker groups to the map
